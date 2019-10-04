bdcbf9b2d7a59db873f049931202d249867b3363
maxemi <56158358+maxemi@users.noreply.github.com>
2019-10-04 18:45:44 +0700

    Create start-ex.sh

---
+++ start-ex.sh
@@ -0,0 +1,52 @@
+# Coded by. M4X3MiL_SYSTEM
+# Jangan di recode ya gayn, makasih
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+echo
+clear
+echo
+echo $i"•••••••••••••••••••••••••••••••••"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•" $cy"______█████████"                    "$pur Whats Exploiter"
+echo $i"•" $cy"______█▄█████▄█"
+echo $i"•" $cy"______█▼▼▼▼▼█"                      "$i  Author : Maxemi"
+echo $i"•" $cy"_____██▌_______██▌"    "$bi Team   : Indonesia Cyber Muslim"
+echo $i"•" $cy"______█▲▲▲▲▲█"                      "$ku Contact: DM ME !"
+echo $i"•" $cy"______█████████"                  "$me Github : https:github.com/maxemi"
+echo $i"•" $cy"_______██_____██"                 "$pu Youtube: M4X3MiL_SYSTEM"
+echo $i"•" $cy"◕ ▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬ ◕"
+echo $i"•••••••••••••••••••••••••••••••••"
+sleep 2
+echo
+echo $cy"###############"
+echo $cy"# 1.Laki-Laki #"
+echo $cy"# 2.Perempuan #"
+echo $cy"###############"
+echo
+echo $bi"Pilih Jenis Kelamin anda"
+read -p "--•>" pil
+
+if [ $pil = '1' ]
+then
+sh L.sh
+fi
+
+if [ $pil = '2' ]
+then
+sh P.sh
+fi
